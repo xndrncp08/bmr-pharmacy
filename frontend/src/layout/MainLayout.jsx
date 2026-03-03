dd48204fb00e0f65c26c308a7f71c2a3a3a10d0f
xder-08 <xander.rancap@edu.sait.ca>
2026-03-03 03:09:34 -0700

    feat: wire ToastContext and ToastContainer into MainLayout

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,15 +1,23 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import ToastContainer from '../components/Toast/ToastContainer'
+import { useToast } from '../hooks/useToast'
+import { ToastContext } from '../context/ToastContext'
 
 export default function MainLayout() {
+  const { toasts, toast, removeToast } = useToast()
+
   return (
-    <div className="flex min-h-screen bg-surface-50">
-      <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <div className="max-w-6xl mx-auto p-8 page-enter">
-          <Outlet />
-        </div>
-      </main>
-    </div>
+    <ToastContext.Provider value={toast}>
+      <div className="flex min-h-screen bg-surface-50">
+        <Sidebar />
+        <main className="flex-1 overflow-auto">
+          <div className="max-w-6xl mx-auto p-8 page-enter">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+      <ToastContainer toasts={toasts} onRemove={removeToast} />
+    </ToastContext.Provider>
   )
 }
